e2b517115e1d8a7c61bbce7c906ff4a96e6f8a1b
chencuixiang2019 <52023872+chencuixiang2019@users.noreply.github.com>
2020-08-05 14:51:27 +0800

    Add files via upload

---
+++ mamahaoVue/src/components/tablepage/ToolHome.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dashboard-editor-container">
+    <div>
+      <el-row :gutter="20">
+        <el-col :span="3">
+          <clear-phone-tool></clear-phone-tool>
+        </el-col>
+        <el-col :span="3">
+          <check-commodity-tool></check-commodity-tool>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="22">
+          <show-vcode-tool></show-vcode-tool>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import CheckCommodityTool from './components/CheckCommodityTool'
+  import ClearPhoneTool from './components/ClearPhoneTool'
+  import ShowVcodeTool from './components/ShowVcodeTool'
+
+  export default {
+    name: "ToolHome",
+    components: {CheckCommodityTool, ClearPhoneTool, ShowVcodeTool},
+  }
+</script>
+
+<style lang="scss" scoped>
+  .dashboard-editor-container {
+    padding: 32px;
+    background-color: rgb(240, 242, 245);
+    position: relative;
+
+    .github-corner {
+      position: absolute;
+      top: 0px;
+      border: 0;
+      right: 0;
+    }
+
+    .chart-wrapper {
+      background: #fff;
+      padding: 16px 16px 0;
+      margin-bottom: 32px;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    .chart-wrapper {
+      padding: 8px;
+    }
+  }
+</style>
---
+++ mamahaoVue/src/components/tablepage/tablepage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>常用接口，工具等等</div>
+</template>
+
+<script>
+  export default {
+    name: "tablepage"
+  }
+</script>
+
+<style scoped>
+
+</style>
